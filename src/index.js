205c9576316744495b93df3fd852493979e6f943
liyushen <842303863@qq.com>
2021-11-01 14:50:06 +0800

    feat: init

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import * as PIXI from 'pixi.js';
+
+console.log('hello canvas unit test');
+console.log('pixi', PIXI);
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'bundle.js',
+    },
+    module: {
+        rules: [
+            { test: '/\.js$/', use: 'js-loader' },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './view/index.html',
+        }),
+    ],
+    devServer: {
+        host: '0.0.0.0',
+        port: 9000,
+    },
+}
